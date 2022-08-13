32195e241314bb1c33770a7dc2726007b0bcb539
programmer24680 <69705177+programmer24680@users.noreply.github.com>
2022-08-13 21:09:11 +0530

    Add functionality to re-run the program.
    Add functionality to re-run the program without re-opening the program.

--- src/SpamBot.py
+++ src/SpamBot.py
@@ -1,19 +1,30 @@
 import pyautogui
 import os
 import time
-word = input("Which word do you want to spam: ")
-i = input("How many times do you want to spam: ")
-print("The program will now wait for 5 secs. In this time, open the social media app where you want to spam and select the textbox.")
-time.sleep(5)
-j = int(i)
-k = 1
+while True:
+	word = input("Which word do you want to spam: ")
+	i = input("How many times do you want to spam: ")
+	print("The program will now wait for 5 secs. In this time, open the social media app where you want to spam and select the textbox.")
+	time.sleep(5)
+	j = int(i)
+	k = 1
 
-while k<=j:
-	pyautogui.typewrite(word)
-	pyautogui.press('enter')
-	k=k+1
+	while k<=j:
+		pyautogui.typewrite(word)
+		pyautogui.press('enter')
+		k=k+1
 
-
-print("Thanks for using this program")
-print("Operation completed successfully. The program will now exit.")
-os.system("pause")
+	print("Operation completed successfully.")
+	repetion = input("Do you want to use the program again(Y/N): ").upper()
+	if repetion == "Y":
+		continue
+	elif repetion == "N":
+		print("Thanks for using this program")
+		print("The program will now exit.")
+		time.sleep(2)
+		break
+	else:
+		print("You have to enter Y/N only.")
+		print("The program will now exit.")
+		time.sleep(2)
+		break
